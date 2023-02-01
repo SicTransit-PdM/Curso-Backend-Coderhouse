bc6b291ff5601c96d82516553e5775323a00432f
Paulo Morel <morelpaulo89@gmail.com>
2023-01-31 22:57:10 -0300

    Desafio 2

--- Desafio entregable 1/app.js
+++ Desafio entregable 1/app.js
@@ -1,18 +1,19 @@
-import Product from './models/Product.js'
 import ProductManager from './models/ProductManager.js'
 
 const adm = new ProductManager
+const test = async () => {
 
-const producto1 = new Product('Campera', 'Color azul', 600, 'thumbnail-link', '1241359301', 23)
-const producto2 = new Product('Remera', 'Color negro', 800, 'thumbnail-link', '4918274987', 32)
-const producto3 = new Product('Remera con codigo repetido', 'Color verde', 872, 'thumbnail-link', '4918274987', 41)
+    await adm.addProduct('Campera', 'Color azul', 600, 'thumbnail-link', '1241359301', 23)
+    await adm.addProduct('Campera con un campo menos', 'Color azul', 600, 'thumbnail-link', '124135163')
+    await adm.addProduct('Campera con un campo vacio', '', 600, 'thumbnail-link', '12514', 12)
+    await adm.addProduct('Remera a modificar color', 'Color negro', 800, 'thumbnail-link', '4918274987', 32)
+    await adm.addProduct('Remera con codigo repetido', 'Color verde', 872, 'thumbnail-link', '4918274987', 41)
+    await adm.addProduct('Pantalon a eliminar', 'Color amarillo', 152, 'thumbnail-link', '346345431', 4132)
+    console.log(adm.getProducts())
+    let id = 2
+    await adm.getProductById(id)
 
-//const productos = [producto1, producto2, producto3]
-
-adm.addProduct(producto1)
-adm.addProduct(producto2)
-adm.addProduct(producto3)
-const id = 2
-//console.log('Administrador =>', adm)
-console.log('Obtener productos =>', adm.getProducts())
-console.log(`Obtener producto por ID: id=${id} =>`, adm.getProductById(id))
+    await adm.updateProduct(2, null, 'Color violeta')
+    await adm.deleteProduct(3)
+}
+test()
--- Desafio entregable 1/models/ProductManager.js
+++ Desafio entregable 1/models/ProductManager.js
@@ -1,25 +1,74 @@
+import Product from "./Product.js"
+import fs from 'fs/promises'
+
 class ProductManager {
     constructor(){
-        this.products = []
+        this.path = './data/products.JSON'
     }
-    // Metodos
-    addProduct(product){
-        console.log(`Verificación de ${product.title} =>`, 'Código: ', product.code, 'Verificación:', this.products.some(p => p.code == product.code))
-        if(this.products.some(p => p.code == product.code)){
-            console.error(`The product "${product.title}" is already added to the list.`)
+    // MÉTODOS
+    async getProducts(){
+        const read = await fs.readFile(this.path, 'utf-8')
+        const data = (read == '') ? [] : JSON.parse(read)
+        return data
+    }
+    async getProductById(id){
+        let data = await this.getProducts()
+        let result = data.find(p => p.id == id) ?? console.log(`No results found matching "ID: ${id}"`)
+        return result
+    }
+    async addProduct(title, description, price, thumbnail, code, stock){
+        console.log('--------------------------')
+        console.log(`CHECKING "${title}":`)
+        const data = await this.getProducts()
+        // Comprobación del código del producto
+        let invalidCode = data.some(p => p.code == code)
+        console.log(`"${title}" code is invalid? =>`, invalidCode)
+        if(invalidCode){
+            return console.error(`ERROR: "${title}" is already added to the list.`)
         } else {
-            if(Object.values(product).includes(undefined)){
-                console.error('All fields are required')
+            // Comprobación de los campos del producto
+            let invalidArgs = Array.from(arguments).includes(undefined) || Array.from(arguments).length != 6 || Array.from(arguments).includes('')
+            console.log(`"${title}" has invalid fields? =>`, invalidArgs)
+            if(invalidArgs){
+                return console.error('ERROR: All fields are required')
             } else {
-            this.products.push(product)
-            }
+                // Adición del producto a la lista
+                data.push(new Product(title, description, price, thumbnail, code, stock))
+                console.log(`"${title}" was added to the list`)
+                await fs.writeFile(this.path, JSON.stringify(data, null, '\t'))
+                }
         }
     }
-    getProducts(){
-        return this.products
+    async updateProduct(id, title, description, price, thumbnail, code, stock){
+        const data = await this.getProducts()
+        let toUpdate = await this.getProductById(id)
+        if (toUpdate){
+            console.log('--------------------------')
+            console.log(`UPDATING ${toUpdate.title}`)
+            let index = data.findIndex(p => p.id == id)
+            toUpdate.title = title ?? toUpdate.title
+            toUpdate.description = description ?? toUpdate.description
+            toUpdate.price = price ?? toUpdate.price
+            toUpdate.thumbnail = thumbnail ?? toUpdate.thumbnail
+            toUpdate.code = code ?? toUpdate.code
+            toUpdate.stock = stock ?? toUpdate.stock
+            data.splice(index, 1, toUpdate)
+            await fs.writeFile(this.path, JSON.stringify(data, null, '\t'))
+        } else {
+            return console.error('ERROR: No data matches to update found')
+        }
     }
-    getProductById(id){
-        return this.products.find(p => p.id == id) ?? console.error('Not found')
+
+    async deleteProduct(id){
+        const data = await this.getProducts()
+        const toDelete = await this.getProductById(id)
+        if (toDelete){
+            console.log('--------------------------')
+            console.log(`DELETING ${toDelete.title}`)
+            let index = data.findIndex(p => p.id == id)
+            data.splice(index, 1)
+            await fs.writeFile(this.path, JSON.stringify(data, null, '\t'))
+        }
     }
 }
 
